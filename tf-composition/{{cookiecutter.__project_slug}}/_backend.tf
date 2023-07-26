4f42afc7ead7393f83b9b6019262d251bcae67f4
Khalshim <mhalitim@gmail.com>
2023-07-26 15:01:27 +0200

    feat(terraform): :sparkles: add s3 backend and remove module and simplify tf-module

--- tf-composition/{{cookiecutter.__project_slug}}/_backend.tf
+++ tf-composition/{{cookiecutter.__project_slug}}/_backend.tf
@@ -1,3 +1,12 @@
-terraform {
-  backend "local" {}
+# Exemple to test with local tfstate
+# For dev env:
+# terraform {
+#   backend "local" {
+#     path = "./env/dev/terraform.tfstate"
+#     workspace_dir = "./env/dev"
+#   }
+# }
+
+teraform {
+  backend "s3" {}
 }
--- tf-module/{{cookiecutter.__project_slug}}/_modules/README.md
+++
@@ -1,1 +0,0 @@
-<!-- clone a local module and test it locally by adding it in _override.tf -->
