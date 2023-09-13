6d756455f36079025d147c45166a26e75849bfe3
Khalshim <mhalitim@gmail.com>
2023-09-13 21:20:07 +0200

    fix(python-scripts): :white_check_mark: python scripts cookiecutter ready
    * feat(python-scripts): :construction_worker: add pre-commit and github actions scripts

---
+++ python-scripts/{{cookiecutter.__project_slug}}/src/{{cookiecutter.__folder_name_formatted}}/{{ cookiecutter.__first_script_name_formatted }}.py
@@ -0,0 +1,45 @@
+import logging
+logging.getLogger().setLevel(logging.DEBUG)
+
+from . import config
+
+def _{{cookiecutter.__first_script_name_formatted}}(argA, argB):
+  return {
+    "argA": argA,
+    "argB": argB,
+  }
+
+def main():
+  import argparse
+
+  script_parser = argparse.ArgumentParser(
+    prog='{{cookiecutter.__first_script_name_formatted}}',
+    description='{{cookiecutter.__first_script_name_formatted}} script',
+  )
+
+  script_parser.add_argument(
+    "-a",
+    "--argA",
+    action="store",
+    metavar="arg1",
+    help="{{cookiecutter.__first_script_name_formatted}} argA",
+    required=True,
+  )
+
+  script_parser.add_argument(
+    "-b",
+    "--argB",
+    action="store",
+    metavar="color",
+    help="{{cookiecutter.__first_script_name_formatted}} color",
+    required=True,
+  )
+
+  # Example how to access config toml file
+  logging.debug(config)
+
+  # Using **vars() to convert Namespace to dict
+  logging.debug(_{{cookiecutter.__first_script_name_formatted}}(**vars(script_parser.parse_args())))
+
+if __name__ == '__main__':
+  main()
