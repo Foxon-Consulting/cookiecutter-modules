bc0db0836bf9680ea526e0797b7278fb6cdd43d9
Khalshim <mhalitim@gmail.com>
2023-09-26 11:03:39 -0400

    feat(python-scripts): :sparkles: finish python scripts full automated (#21)
    * feat(python-scripts): :sparkles: add setup and extension setting to facilitate

--- python-scripts/{{cookiecutter.__project_slug}}/src/{{cookiecutter.__folder_name_formatted}}/{{ cookiecutter.__first_script_name_formatted }}.py
+++ python-scripts/{{cookiecutter.__project_slug}}/src/{{cookiecutter.__folder_name_formatted}}/{{ cookiecutter.__first_script_name_formatted }}.py
@@ -1,45 +1,53 @@
 import logging
+
 logging.getLogger().setLevel(logging.DEBUG)
 
 from . import config
 
+
 def _{{cookiecutter.__first_script_name_formatted}}(argA, argB):
-  return {
-    "argA": argA,
-    "argB": argB,
-  }
+    return {
+        "argA": argA,
+        "argB": argB,
+    }
+
 
 def main():
-  import argparse
+    import argparse
 
-  script_parser = argparse.ArgumentParser(
-    prog='{{cookiecutter.__first_script_name_formatted}}',
-    description='{{cookiecutter.__first_script_name_formatted}} script',
-  )
+    script_parser = argparse.ArgumentParser(
+        prog="{{cookiecutter.__first_script_name_formatted}}",
+        description="{{cookiecutter.__first_script_name_formatted}} script",
+    )
 
-  script_parser.add_argument(
-    "-a",
-    "--argA",
-    action="store",
-    metavar="arg1",
-    help="{{cookiecutter.__first_script_name_formatted}} argA",
-    required=True,
-  )
+    script_parser.add_argument(
+        "-a",
+        "--argA",
+        action="store",
+        metavar="arg1",
+        help="{{cookiecutter.__first_script_name_formatted}} argA",
+        required=True,
+    )
 
-  script_parser.add_argument(
-    "-b",
-    "--argB",
-    action="store",
-    metavar="color",
-    help="{{cookiecutter.__first_script_name_formatted}} color",
-    required=True,
-  )
+    script_parser.add_argument(
+        "-b",
+        "--argB",
+        action="store",
+        metavar="color",
+        help="{{cookiecutter.__first_script_name_formatted}} color",
+        required=True,
+    )
 
-  # Example how to access config toml file
-  logging.debug(config)
+    result = _{{cookiecutter.__first_script_name_formatted}}(**vars(script_parser.parse_args()))
 
-  # Using **vars() to convert Namespace to dict
-  logging.debug(_{{cookiecutter.__first_script_name_formatted}}(**vars(script_parser.parse_args())))
+    # Example how to access config toml file
+    logging.debug(config)
 
-if __name__ == '__main__':
-  main()
+    # Using **vars() to convert Namespace to dict
+    logging.debug(result)
+
+    return result
+
+
+if __name__ == "__main__":
+    main()
---
+++ python-scripts/{{cookiecutter.__project_slug}}/tests/test_{{cookiecutter.__first_script_name_formatted}}.py
@@ -0,0 +1,18 @@
+import unittest
+
+from {{cookiecutter.folder_name}}.{{cookiecutter.__first_script_name_formatted}} import _{{cookiecutter.__first_script_name_formatted}}
+
+
+class {{cookiecutter.__first_script_test_name_formatted}}Test(unittest.TestCase):
+    def setUp(self):
+        # self.task = Task("Dummy task", False)
+        pass
+
+    def tearDown(self):
+        # To be implemented if required
+        pass
+
+    def test_{{cookiecutter.__first_script_name_formatted}}(self):
+        self.assertEqual(
+            _{{cookiecutter.__first_script_name_formatted}}("green", "blue"), {"argA": "green", "argB": "blue"}
+        )
