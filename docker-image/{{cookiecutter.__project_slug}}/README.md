adea042462cfe99c7d9b3a3be621c46273b55fdb
mhalitim <mhalitim@gmail.com>
2024-03-19 17:57:19 +0100

    feat: :recycle: clean and refactor all cookiecutter

--- docker-image/{{cookiecutter.__project_slug}}/README.md
+++
@@ -1,6 +0,0 @@
-# README
-
-## Quick setup of environment:
-Use ```./setup.ps1``` or ```setup.sh``` to fast setup your environment. It will:
-
-- TODO
--- docker-image/{{cookiecutter.__project_slug}}/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Initialize the repository
-git init
-git add .
-git commit -m "feat!: :tada: cookiecutter scaffold"
-
-pre-commit install
